ebe1dcafe4bfe0c25d1cc728ccd1451e5f95cb5a
ZeroELopez <60636555+ZeroELopez@users.noreply.github.com>
2024-02-05 15:55:29 -0500

    Update Items

---
+++ Assets/Items/Food.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Food : IItem
+{
+    public string name { get; private set; }
+
+    public int basePrice { get; private set; }
+
+    public void Pocket(PlayerScript player) =>
+        player.inventory.Add(this);
+
+    float rotation = 0;
+    int pause = 0;
+    int damage = 20;
+    public void Remove()
+    {
+    }
+
+    public void SteppedOn(PlayerScript player)
+    {
+
+    }
+
+    public void Use(PlayerScript player)
+    {
+
+    }
+
+
+}
